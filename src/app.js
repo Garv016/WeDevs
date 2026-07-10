f1d7627fe9110d37b09711cb5844385866a8fe34
Garv <garvchawla01@gmail.com>
2026-07-10 21:18:34 +0530

    parameters and queries

--- src/app.js
+++ src/app.js
@@ -2,24 +2,16 @@ const express = require("express")
 
 const app = express();
 
-// matches /abc and /ab
-
-// Doesnt work for express 5 ver
-// app.use("/ab?c" , (req,res) => {
-//     res.send("This is abc")
-// })
-
-// Routing regex
-
-app.use(/^\/abc?$/, (req, res) => {
-    console.log(req.query   )
-    res.send("Matched /ab or /abc");
+app.use("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+    
+    res.send({
+        message: "This is user GET only",
+        firstName: "Garv",
+        netWorth: "1.2 Trillion dollars"
+    });
 });
-
-app.use(/^\/wx.*yz$/, (req, res) => {
-    res.send("Matched!");
-});
-
 const port = 7777
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
