36a6221724064b4f26c80123e6ad291cca18cd89
Garv <garvchawla01@gmail.com>
2026-07-14 02:43:57 +0530

    GET. FEED , DELETE , UPDATE API

--- src/app.js
+++ src/app.js
@@ -9,6 +9,87 @@ const User = require("./model/user");
 // .use() without a route means work for all
 app.use(express.json()) // middleware
 
+// GET /user using mail
+app.get("/user" , async (req,res) =>{
+    try{
+        const userEmail = await User.findOne({email : req.body.email})
+        // findOne gives one back find gives all that correspond with the requirement
+        if(userEmail.length === 0){
+            res.status(404).send("User not found")
+        }
+        else res.send(userEmail)
+
+    }
+    catch(err){
+        res.send("Something's OFF");
+    }
+})
+
+
+// FEED API - GET all the users info
+app.get("/feed" , async (req,res) =>{
+    try{
+        const userData= await User.find({}) 
+        if(userData.length === 0){
+            
+            res.status(404).send("No user")
+        }
+        else {
+            res.send(userData)
+            console.log("ALL users fetched successfully");
+        }
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Something's OFF");
+    }
+})
+
+// DELETE API using id
+app.delete("/delete" , async (req,res) =>{
+    try{
+        // const userData= await User.findById({_id : req.body.id}) 
+        const userData= await User.findById(req.body.id) // better way
+        console.log(req.body.id);
+        
+        if(userData.length === 0){
+            res.status(404).send("No user")
+        }
+        else {
+            res.send(userData)
+            await userData.deleteOne() // deleteOne() deletes the first document that matches the filter.   
+            console.log("User deleted successfully");
+        }
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Something's OFF");
+    }
+})
+
+
+// Update user API
+    app.patch("/user" , async (req,res) =>{
+        try{
+            const userData= await User.findByIdAndUpdate(req.body.id,req.body) 
+            // can add in above as options { new: true, runValidators: true }
+            if(!userData.length === 0){
+                res.status(404).send("No user")
+            }
+            else {
+                res.send(userData)
+                console.log("User Updated successfully");
+            }
+
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send("Something's OFF");
+        }
+    })
+
 // Taking input from postman
 app.post("/signup" , async (req,res) => {
     // console.log(req.body); // if hadnt used express.json upr then woudve gotten undefined instead of data
